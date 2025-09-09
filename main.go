f37ba6f0144b210536f0d07af04d67efdd4f24e1
Tristan Kruger <Tmkruger60@gmail.com>
2025-09-08 22:40:46 -0700

    text fix + pseudo plans for features

--- main.go
+++ main.go
@@ -7,22 +7,29 @@ import (
 
 func main() {
 	name := intro()
-	fmt.Printf("Hello, %v\n", name)
+	fmt.Printf("Hello, %v\n.", name)
+	//Start finding out what class and stats and such
+	//Choose what level to create to. (max 3 for our sake)
+	//Maybe Use AI or Premade Backstories or someform of backstory creator
+	//Roll up all the data into a full blown character
+	//Output to CLI or Official PDF
 }
 
 func intro() (name string) {
+	// Flavor Text for Introduction :)
 	fmt.Println("\"Life before death, Strength before weakness, Journey before destination\" - Oath of the Knights Radiant")
 	fmt.Println("Odium's forces are growing stronger, we need all the help we can get")
 	fmt.Println("Are you willing to fight with us? (Would you like to make a new character?) y/n")
 	var answer string
 	for {
 		fmt.Scan(&answer)
-		// Determine if we want to make a character
+		//Determine if we want to make a character
 		switch answer {
 		case "n":
 			fmt.Println("We understand, hopefully we will be enough...")
 			os.Exit(0)
 		case "y":
+			//Wants to make character so get name
 			for {
 				fmt.Println("What can we call you? (Character Name)")
 				fmt.Scan(&answer)
@@ -36,7 +43,7 @@ func intro() (name string) {
 						case "n":
 							break
 						default:
-							fmt.Println("y or n only please")
+							fmt.Println("I don't understand, Can you repeat yourself? (y/n only please)")
 							continue
 					}
 					break
