b213036fbee507352b207bba29c3ba2f8bd57003
Tristan Kruger <Tmkruger60@gmail.com>
2025-09-10 11:54:23 -0700

    added GetLevel and some text changes

--- cmd/main.go
+++ cmd/main.go
@@ -2,20 +2,56 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/tmkruger/sl_character_gen/internal/data"
 	"github.com/tmkruger/sl_character_gen/internal/functions"
 )
 
+func intro(c *data.Character, mainMenu *bool) {
+	for *mainMenu == true {
+		fmt.Println("\nWelcome to the Stormlight RPG Character Creator!")
+		fmt.Println("1. Create a new custom character")
+		fmt.Println("2. Load an existing character")
+		fmt.Println("3. Go through ”The First Step” Adventure to make a character")
+		fmt.Println("4. Exit\n")
+		fmt.Print("Enter your choice: ")
+		var choice int
+		fmt.Scanln(&choice)
+		fmt.Print("\033[1A\033[2K")
+		switch choice {
+		case 1:
+			functions.CustomIntro(c, mainMenu)
+		case 2:
+			println("Loading Character...")
+			// functions.LoadExistingCharacter(&c)
+		case 3:
+			println("Starting The First Step Adventure...")
+			//functions.FirstStepAdventure(&c)
+		case 4:
+			fmt.Println("Exiting...")
+			os.Exit(0)
+		default:
+			fmt.Println("Invalid choice!")
+		}
+	}
+}
+
 func main() {
 	c := data.Character{}
-	functions.Intro(&c)
+	mainMenu := true
+	for mainMenu == true {
+		intro(&c, &mainMenu)
+	}
+
 	fmt.Print("\033[1A\033[2K")
 	fmt.Printf("\nHello, %v\n", c.Name)
 	functions.AncestryPicker(&c)
-	
+	functions.GetLevel(&c)
+
 	println(c.Name)
 	println(c.Ancestry)
+	println(c.Level)
 	//Start finding out what class and stats and such
 	//Choose what level to create to. (max 3 for our sake)
 	//Maybe Use AI or Premade Backstories or someform of backstory creator
--- internal/functions/functions.go
+++ internal/functions/functions.go
@@ -2,38 +2,41 @@ package functions
 
 import (
 	"fmt"
-	"os"
 	"strings"
 
 	"github.com/tmkruger/sl_character_gen/internal/data"
 )
 
-
-func Intro(c *data.Character) {
+func CustomIntro(c *data.Character, mainMenu *bool) {
 	// Flavor Text for Introduction :)
 	fmt.Println("”Life before death, Strength before weakness, Journey before destination”\n	- Oath of the Knights Radiant\n")
 	fmt.Println("Odium's forces are growing stronger, we need all the help we can get")
-	fmt.Println("Are you willing to fight with us? (Would you like to make a new character?) y/n\n")
+	fmt.Println("Are you willing to fight with us? (Would you like to make a new character? (y/n))")
 	var answer string
 	for {
+		fmt.Print("Enter your choice: ")
 		fmt.Scan(&answer)
 		//Determine if we want to make a character
 		switch answer {
 		case "n":
 			fmt.Print("\033[1A\033[2K")
-			fmt.Println("We understand, hopefully we will be enough...")
-			os.Exit(0)
+			fmt.Println("We understand, hopefully we will be enough...\n")
+			return
 		case "y":
 			//Wants to make character so get name
+			*mainMenu = false
 			for {
 				fmt.Print("\033[1A\033[2K")
 				fmt.Println("What can we call you? (Character Name)")
+				fmt.Print("Enter your character name: ")
 				fmt.Scan(&answer)
 				name := answer
 				fmt.Print("\033[1A\033[2K")
 				fmt.Printf("Are you certain %v is your name? (y/n)\n", name)
 				for {
+					fmt.Print("Enter your choice: ")
 					fmt.Scan(&answer)
+					fmt.Print("\033[1A\033[2K")
 					switch answer {
 					case "y":
 						c.Name = name
@@ -60,6 +63,7 @@ func AncestryPicker(c *data.Character) {
 	println("1.Human\n2.Singer")
 	var answer string
 	for {
+		fmt.Print("Enter your choice: ")
 		fmt.Scan(&answer)
 		answer = strings.ToLower(answer)
 		//Gets rid of what user typed as to not break immersion
@@ -77,14 +81,37 @@ func AncestryPicker(c *data.Character) {
 			fmt.Println("I don't understand, Can you repeat yourself? (Items from the list only please)")
 		}
 	}
-	return
 }
 
-//func GetLevel() 
+func GetLevel(c *data.Character) {
+	println("How experienced are you? (Starting Level 1-3)")
+	for {
+		fmt.Print("Enter your choice: ")
+		var choice int
+		fmt.Scanln(&choice)
+		fmt.Print("\033[1A\033[2K")
+		switch choice {
+		case 1:
+			c.Level = 1
+			return
+		case 2:
+			c.Level = 2
+			return
+		case 3:
+			c.Level = 3
+			return
+		default:
+			fmt.Println("I don't understand, Can you repeat yourself? (Items from the list only please)")
+		}
+	}
+}
 
+func GetPaths(c *data.Character) {
+	//Choose paths depending on level
+}
 
-			//println("Which kingdom did you grow up in? (Nationality)")
-			//fmt.Println("1.Alethi	5.Reshi		9.Thaylen\n2.Azish 	6.Shin		10.Unkalaki\n3.Herdazian	7.Iriali	11.Veden\n4.Natan		8.Kharbranthian")
+//println("Which kingdom did you grow up in? (Nationality)")
+//fmt.Println("1.Alethi	5.Reshi		9.Thaylen\n2.Azish 	6.Shin		10.Unkalaki\n3.Herdazian	7.Iriali	11.Veden\n4.Natan		8.Kharbranthian")
 
-			//println("What forms do you know?")
-			//println("1.Finesse (Art Form | Nimble Form)\n2.Wisdom (Mediation Form | Scholar Form)\n3.Resolve (War Form | Work Form)")
+//println("What forms do you know?")
+//println("1.Finesse (Art Form | Nimble Form)\n2.Wisdom (Mediation Form | Scholar Form)\n3.Resolve (War Form | Work Form)")
